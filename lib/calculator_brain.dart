e841e5d07bcdb0ad41cc46e494fbbeabd87c2870
Shreshth Srivastava <shreshth28srivastava@gmail.com>
2019-08-12 00:35:00 +0530

    Few Tweaks and fixed Consister underweight issue

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -6,11 +6,11 @@ class CalculatorBrain {
   final int height;
   final int weight;
 
-  double _bmi=0;
-  CalculatorBrain({@required this.height,@required this.weight});
+  double _bmi;
+  CalculatorBrain({@required this.height, @required this.weight});
 
   String calculateBMI() {
-    double _bmi = weight / pow(height / 100, 2);
+    _bmi = weight / pow(height / 100, 2);
     return (_bmi.toStringAsFixed(1));
   }
 
@@ -30,7 +30,7 @@ class CalculatorBrain {
     } else if (_bmi >= 18.5) {
       return ('You have a normal body weight. Good job!');
     } else {
-      return ('You have a lower than noraml body weight. You can eat a bit more');
+      return ('You have a lower than normal body weight. You can eat a bit more');
     }
   }
 }
